3f2298bbf79b79c1cc9350a9015dd6aae4d20743
Evan Bacon <baconbrix@gmail.com>
2019-04-10 19:32:57 -0700

    Updated to use es modules in web

---
+++ src/index.web.js
@@ -0,0 +1,15 @@
+export { default as createDrawerNavigator } from './navigators/createDrawerNavigator';
+
+/**
+ * Router
+ */
+export { default as DrawerRouter } from './routers/DrawerRouter';
+export { default as DrawerActions } from './routers/DrawerActions';
+
+/**
+ * Views
+ */
+export { default as DrawerNavigatorItems } from './views/DrawerNavigatorItems';
+export { default as DrawerSidebar } from './views/DrawerSidebar';
+export { default as DrawerView } from './views/DrawerView';
+export { default as DrawerGestureContext } from './utils/DrawerGestureContext';
